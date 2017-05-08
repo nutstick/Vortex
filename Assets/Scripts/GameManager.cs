5f513037b06f48b4c4b9854fe33a8ca7908194a9
nutstick <nuttapat.kirawittaya@gmail.com>
2017-05-08 16:09:30 +0700

    Add game scene

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public Text gameAdvice;
+    public Terrain terrain;
+    public float startWait;
+
+    IEnumerator FindTerrain()
+    {
+        yield return new WaitForSecond(startWait);
+
+        gameAdvice = "Please capture Terrian pattern.";
+        gameAdvice.setActive();
+        
+        while (true)
+        {
+            if (terrain.isShow)
+            {
+                break;
+            }
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
